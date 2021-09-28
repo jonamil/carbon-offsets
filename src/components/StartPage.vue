1e2c5e1ea9b07d4b0bc669377eac6df556b2f249
Jona <8698248+jonamil@users.noreply.github.com>
2021-09-28 13:13:11 +0200

    Add animated StartPage images

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -5,18 +5,37 @@
       <h1>Carbon Neutrality:<br>A Label For Sale?</h1>
       <h3>An introduction to carbon offsets:<br>how they work and where they fail</h3>
     </div>
+    <div class="prompt">Scroll to begin</div>
+    <InlineSvg :src="pathToImage('corner-left')" />
+    <InlineSvg :src="pathToImage('corner-right')" />
   </section>
 </template>
 
 <script>
+import InlineSvg from 'vue-inline-svg';
+
 export default {
   name: 'StartPage',
   inheritAttrs: false,
+
+  components: {
+    InlineSvg
+  },
 
   props: {
     class: {
       type: String,
       default: ''
+    }
+  },
+
+  methods: {
+    pathToImage(filename) {
+      try {
+        return require('@/assets/start/' + filename + '.svg');
+      } catch (error) {
+        return '';
+      }
     }
   }
 }
@@ -26,12 +45,6 @@ export default {
 @import '~@/assets/variables.scss';
 
 section {
-  background-image: url('~@/assets/start/corner-left.svg'), url('~@/assets/start/corner-right.svg');
-  background-position: 1.5rem bottom, calc(100% - 3.0rem) bottom;
-  background-repeat: no-repeat;
-  background-size: 35.6rem, 38.2rem;
-  // background-size: calc(100vw / 1200 * 356), calc(100vw / 1200 * 382);
-
   h4 {
     position: absolute;
     top: 1.3rem;
@@ -66,5 +79,113 @@ section {
       line-height: 1.2;
     }
   }
+
+  .prompt {
+    position: absolute;
+    bottom: 5.0rem;
+    left: 0;
+    right: 0;
+    text-align: center;
+    font-size: 1.6rem;
+    font-weight: $weight-semibold;
+    letter-spacing: 0.1rem;
+    text-transform: uppercase;
+    color: $color-lighter;
+
+    &:after {
+      position: absolute;
+      display: block;
+      content: '';
+      bottom: -3.7rem;
+      left: 0;
+      right: 0;
+      height: 3.2rem;
+      opacity: 0.25;
+      background: url('~@/assets/icons/nav-down.svg') center no-repeat;
+      background-size: 3.2rem;
+      animation: arrow-down 0.8s ease-in-out infinite alternate;
+    }
+  }
+
+  svg {
+    position: absolute;
+    bottom: 0;
+
+    &:first-of-type {
+      &:deep(path.smoke) {
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+        animation-direction: reverse;
+
+        &.home { // 277.5
+          stroke-dasharray: 377.5 5.25;
+          animation-name: smoke-home;
+          animation-duration: 7.655s;
+          animation-delay: 2s;
+        }
+
+        &.highrise-front { // 298.5
+          stroke-dasharray: 398.5 5.25;
+          animation-name: smoke-highrise-front;
+          animation-duration: 8.075s;
+          animation-delay: 4.5s;
+        }
+
+        &.highrise-back { // 616.5
+          stroke-dasharray: 716.5 5.25;
+          animation-name: smoke-highrise-back;
+          animation-duration: 14.435s;
+          animation-delay: 0.5s;
+        }
+
+        &.plane { // 198
+          stroke-dasharray: 298 5.25;
+          animation-name: smoke-plane;
+          animation-duration: 6.065s;
+          animation-delay: 3s;
+        }
+      }
+    }
+
+    &:last-of-type {
+      right: 0;
+
+      &:deep(g) {
+        transform-origin: center;
+        transform-box: fill-box;
+        animation: rotor 10s linear infinite;
+      }
+    }
+  }
+}
+
+@keyframes arrow-down {
+  0%   { transform: translateY(-0.2rem) }
+  100% { transform: translateY(0.2rem) }
+}
+
+@keyframes smoke-home {
+  0%   { stroke-dashoffset: 0 }
+  100% { stroke-dashoffset: 382.75 }
+}
+
+@keyframes smoke-highrise-front {
+  0%   { stroke-dashoffset: 0 }
+  100% { stroke-dashoffset: 403.75 }
+}
+
+@keyframes smoke-highrise-back {
+  0%   { stroke-dashoffset: 0 }
+  100% { stroke-dashoffset: 721.75 }
+}
+
+@keyframes smoke-plane {
+  0%   { stroke-dashoffset: 0 }
+  100% { stroke-dashoffset: 303.25 }
+}
+
+@keyframes rotor {
+  0%   { transform: rotate(0deg) }
+  100% { transform: rotate(360deg) }
 }
 </style>
